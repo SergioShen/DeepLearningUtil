4f98c884f58080955ac177facbf234af326da6fc
Shen Sijie <sjshen@pku.edu.cn>
2018-01-19 06:25:57 +0800

    ADD: Docstring of class *Vocabulary*

--- vocabulary.py
+++ vocabulary.py
@@ -10,7 +10,15 @@ import copy
 
 
 class Vocabulary(object):
+    """
+    Implement of vocabulary
+    """
+
     def __init__(self, reserved_words=None):
+        """
+        Create a vocabulary
+        :param reserved_words: reserved words that won't change
+        """
         self.index_to_word = {}
         self.word_to_index = {}
         self.reserved_words = []
@@ -22,21 +30,42 @@ class Vocabulary(object):
                 self.add(item)
 
     def size(self):
+        """
+        Get size of vocabulary
+        :return: size of vocabulary
+        """
         return len(self.index_to_word)
 
     def get_index(self, word, default=None):
+        """
+        Get index from word
+        :param word: input word
+        :param default: return value when *word* is not in vocabulary
+        :return: index of *word*, or *default* if *word* is not in the vocabulary
+        """
         try:
             return self.word_to_index[word]
         except KeyError:
             return default
 
     def get_word(self, index, default=None):
+        """
+        Get word from index
+        :param index: input index
+        :param default: return value when *index* is out of boundary
+        :return: word linked to *index*, or *default* if *index* is out of boundary
+        """
         try:
             return self.index_to_word[index]
         except KeyError:
             return default
 
     def add(self, word):
+        """
+        Add a word into vocabulary
+        :param word: word to add
+        :return: index of the just-added word
+        """
         if word in self.word_to_index:
             self.word_to_frequency[word] += 1
             index = self.word_to_index[word]
@@ -48,9 +77,18 @@ class Vocabulary(object):
         return index
 
     def get_all_words(self):
+        """
+        Get all the words in the vocabulary
+        :return: a list of all the words in the vocabulary
+        """
         return self.word_to_index.keys()
 
     def delete_low_frequency_words(self, threshold):
+        """
+        Delete all the words with frequency lower than *threshold*
+        :param threshold: threshold of word frequency
+        :return: None
+        """
         not_reserved = list(filter(lambda word: word not in self.reserved_words, self.word_to_index.keys()))
         words_to_keep = list(filter(lambda word: self.word_to_frequency[word] >= threshold, not_reserved))
 
@@ -65,6 +103,10 @@ class Vocabulary(object):
         self.word_to_frequency = {word: self.word_to_frequency[word] for word in self.word_to_index.keys()}
 
     def sort(self):
+        """
+        Sort the vocabulary, with reserved words unchanged
+        :return: None
+        """
         not_reserved = list(filter(lambda key: key not in self.reserved_words, self.word_to_index.keys()))
         not_reserved.sort()
 
