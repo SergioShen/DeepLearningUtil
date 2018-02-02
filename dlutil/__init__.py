918fa5188dee79b5cce713fac1ee7eeb65b0d083
Shen Sijie <sjshen@pku.edu.cn>
2018-02-03 00:23:52 +0800

    ADD: *dlutil* package

---
+++ dlutil/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 0:22 2018/2/3
+# @Author: Shen Sijie
+# @File: __init__.py
+# @Project: DeepLearningTemplates
+
+__all__ = ['data_set', 'logger', 'vocabulary']
+from dlutil import *
